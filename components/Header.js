5317d9b830324b64872b867aba5099e5fa7ec46b
SHILAJIRU-dev <ianshilajiru@gmail.com>
2025-07-12 23:43:36 +0300

    New

--- components/Header.js
+++ components/Header.js
@@ -15,7 +15,7 @@ export default async function Header() {
           <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" viewBox="0 0 24 24" fill="currentColor">
             <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z" />
           </svg>
-          <span>All Starz</span>
+          <span>All Starz Dance Company</span>
         </Link>
         
         <nav className="hidden md:flex items-center space-x-6">
